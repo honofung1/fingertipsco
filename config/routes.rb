06401b6648331ae9ff1d3bde01ca7ef0082208a2
Hono1 <tonyfung03051023@gmail.com>
2022-05-12 23:08:41 +0900

    admin page login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root 'dashboards#index', as: :root
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
+
 end
