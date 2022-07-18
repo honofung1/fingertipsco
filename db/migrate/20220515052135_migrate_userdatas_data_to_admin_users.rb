c76b9527468834c12b43b1f1798628d23b9f4b65
Hono1 <tonyfung03051023@gmail.com>
2022-07-18 15:10:28 +0900

    temp to remove migrating old data to new admin table

--- db/migrate/20220515052135_migrate_userdatas_data_to_admin_users.rb
+++ db/migrate/20220515052135_migrate_userdatas_data_to_admin_users.rb
@@ -1,15 +1,15 @@
 class MigrateUserdatasDataToAdminUsers < ActiveRecord::Migration[5.2]
   def up
-    results = ActiveRecord::Base.connection.exec_query("SELECT * FROM USERDATA;")
+    # results = ActiveRecord::Base.connection.exec_query("SELECT * FROM USERDATA;")
 
-    results.each do |result|
-      Admin.create!(
-        name: result["name"],
-        username: result["username"],
-        password: result["password"],
-        password_confirmation: result["password"]
-      )
-    end
+    # results.each do |result|
+    #   Admin.create!(
+    #     name: result["name"],
+    #     username: result["username"],
+    #     password: result["password"],
+    #     password_confirmation: result["password"]
+    #   )
+    # end
   end
 
   def down
