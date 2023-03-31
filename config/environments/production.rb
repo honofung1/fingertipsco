c08b7e9f7bb83f49aeebf5dc529a72dd9f5ca83a
honofung <higashif1023@gmail.com>
2023-03-31 16:01:38 +0900

    [Hotfix] hotfix host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Email setting
-  config.action_mailer.default_url_options = { host: 'ftadmin-9027.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'ftadmin.fingertipsco.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
